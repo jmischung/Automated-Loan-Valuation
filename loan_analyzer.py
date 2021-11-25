eaff51802fe140a23fbf84999acc358d6d9f48ab
Josh Mischung <josh@knoasis.io>
2021-11-25 14:59:50 -0800

    save the results from conditionally filtering lists of loans to a csv file

--- loan_analyzer.py
+++ loan_analyzer.py
@@ -216,4 +216,12 @@ output_path = Path("inexpensive_loans.csv")
 
 # @TODO: Use the csv library and `csv.writer` to write the header row
 # and each row of `loan.values()` from the `inexpensive_loans` list.
-# YOUR CODE HERE!
+with open(output_path, 'w', newline='') as csvfile:
+    # Create the csv writer object.
+    csvwriter = csv.writer(csvfile)
+
+    # Write the header to the CSV file.
+    csvwriter.writerow(header)
+
+    for loan in inexpensive_loans:
+        csvwriter.writerow(loan.values())
